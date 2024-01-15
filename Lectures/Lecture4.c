b02383e559bc8bb615cca444a5b89bea997cd37f
arnabmaj24 <arnabmaj24@gmail.com>
2024-01-15 11:25:55 -0500

    another commit yuh

---
+++ Lectures/Lecture4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 42; // int declare
+    long int la = 1234567891234567; // longer int
+    
+    char c = "a"; // just a char
+
+    char *s = "String"; // string stored as the first characters pointer
+    int p_s = &s; //adress of s
+
+    printf("%s",s);
+
+
+
+
+
+    return 0;
+}
