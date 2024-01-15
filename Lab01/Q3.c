5df8fe5553112d9243a0f7eea0dd2e88c0d20047
arnabmaj24 <arnabmaj24@gmail.com>
2024-01-15 11:44:47 -0500

    c4?

--- Lab01/Q3.c
+++ Lab01/Q3.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
-void change(char *pChange);
+void change(char a[], int len);
 
 
 int main() {
-    // Declare and initialize an array of characters
-    char myArray[] = {'I', 'a', 'm', 'g', 'a', 'y', '\0'};  // '\0' denotes the end of the string
-    // Print the string using printf
-    printf("%s\n", myArray);
+    // // Declare and initialize an array of characters
+    // char myArray[] = {'I', 'a', 'm', 'g', 'a', 'y', '\0'};  // '\0' denotes the end of the string
+    // // Print the string using printf
+    // printf("%s\n", myArray);
 
-    change(&myArray[3]);
+    // change(&myArray[3]);
 
-    printf("%s\n", myArray);
+    // printf("%s\n", myArray);
+
+    char a[100];
+
+    change(a, 100);
+
+    printf(a);
+
 
     return 0;
 }
 
-void change(char *pChange){
-    *pChange = '0';
+void change(char a[], int len){
+    
+    int i = 0;
+    for (i=0;i<len;i++){
+        a[i] = 'f';
+    }
 }
