82ae354bfcf541a758af9b230ac9c82e9ceffe44
arnabmaj24 <arnabmaj24@gmail.com>
2024-01-19 11:23:20 -0500

    another one

--- Lectures/Lecture4.c
+++ Lectures/Lecture4.c
@@ -3,18 +3,30 @@
 int main() {
 
     int a = 42; // int declare
-    long int la = 1234567891234567; // longer int
+    long int la = 1234567897; // longer int
     
-    char c = "a"; // just a char
+    char c = 'f'; // just a char
 
     char *s = "String"; // string stored as the first characters pointer
-    int p_s = &s; //adress of s
+    // int p_s = &s; //adress of s, problem
 
-    printf("%s",s);
+    // -------------------------------------------------------------------
 
+    // arrays and stuff ig
 
+    printf("%d\n", a);
 
+    //double s = sum(1,2,3,45);
+    //printf("%ld\n", s);
 
+    char *str = "hello";
+    //printf("%s\n is stored at adress %ld", str, (long int)str);
+    
+
+    int arr[] = {5,6,7};
+    printf("%ld\n", arr);
+    printf("%d\n", arr[0]);
+    printf("%d\n", *arr+2);
 
     return 0;
 }
